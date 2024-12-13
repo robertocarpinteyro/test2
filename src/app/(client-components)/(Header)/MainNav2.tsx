682a6819d9d1b7e2097bf3d7347be15acdec3e14
Oasis Creativa <roberto.carpinteyro96@gmail.com>
2024-12-13 01:03:49 -0600

    update

--- src/app/(client-components)/(Header)/MainNav2.tsx
+++ src/app/(client-components)/(Header)/MainNav2.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { FC } from "react";
 import Logo from "@/shared/Logo";
 import MenuBar from "@/shared/MenuBar";
@@ -20,18 +20,19 @@ const MainNav2: FC<MainNav2Props> = ({ className = "" }) => {
     <div className={`MainNav2 relative z-10 ${className}`}>
       <div className="px-4 h-20 lg:container flex justify-between">
         <div className="hidden md:flex justify-start flex-1 space-x-3 sm:space-x-8 lg:space-x-10">
-        <Image
-          src="https://res.cloudinary.com/dwrtldhxd/image/upload/v1733722950/logoblack_rdtpxs.png"
-          alt="Separador"
-          width={200} // Ajusta el ancho de la imagen
-          height={100} // Ajusta la altura de la imagen
-          className="contrast-200 brightness-200 self-center h-50 w-20"
-        />
-          
+          <Link
+            href="/"
+            className="relative contrast-200 brightness-200 self-center h-50 w-20"
+          >
+            <Image
+              src="https://res.cloudinary.com/dwrtldhxd/image/upload/v1733722950/logoblack_rdtpxs.png"
+              alt="Logo"
+              width={200} // Ajusta el ancho de la imagen
+              height={100} // Ajusta la altura de la imagen
+            />
+          </Link>
           <div className="hidden lg:block self-center h-10 border-l border-neutral-300 dark:border-neutral-500"></div>
-          <div className="hidden lg:flex ">
-           
-          </div>
+          <div className="hidden lg:flex "></div>
         </div>
 
         <div className="self-center lg:hidden flex-[3] max-w-lg !mx-auto md:px-3">
@@ -40,15 +41,12 @@ const MainNav2: FC<MainNav2Props> = ({ className = "" }) => {
 
         <div className="hidden md:flex flex-shrink-0 justify-end flex-1 lg:flex-none text-neutral-700 dark:text-neutral-100">
           <div className="hidden lg:flex space-x-1">
-       
-           
             <Link
               href={"/listing-stay-detail" as Route<string>}
               className="self-center text-opacity-90 group px-4 py-2 border border-neutral-300 hover:border-neutral-400 dark:border-neutral-700 rounded-full inline-flex items-center text-sm text-gray-700 dark:text-neutral-300 font-medium hover:text-opacity-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
             >
               Chatea con Niddia
             </Link>
-
           </div>
           <div className="flex space-x-2 lg:hidden">
             <NotifyDropdown />
--- src/app/(listing-detail)/layout.tsx
+++ src/app/(listing-detail)/layout.tsx
@@ -57,17 +57,8 @@ const DetailtLayout = ({ children }: { children: ReactNode }) => {
 
       {/* OTHER SECTION */}
       <div className="container py-24 lg:py-32">
-        <div className="relative py-16">
-          <BackgroundSection />
-          <SectionSliderNewCategories
-            heading="Explore by types of stays"
-            subHeading="Explore houses based on 10 types of stays"
-            categoryCardType="card5"
-            itemPerRow={5}
-            sliderStyle="style2"
-          />
-        </div>
-        <SectionSubscribe2 className="pt-24 lg:pt-32" />
+     
+ 
       </div>
 
       {/* STICKY FOOTER MOBILE */}
