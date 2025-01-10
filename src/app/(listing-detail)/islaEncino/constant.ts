7143ecaaf3bd6b5dc172095b93c366a07628d95b
Oasis Creativa <roberto.carpinteyro96@gmail.com>
2025-01-10 02:36:45 -0600

    update

--- src/app/(listing-detail)/islaEncino/constant.ts
+++ src/app/(listing-detail)/islaEncino/constant.ts
@@ -1,10 +1,9 @@
 import { ListingGalleryImage } from "@/components/listing-image-gallery/utils/types";
 
 export const PHOTOS: string[] = [
-  "https://res.cloudinary.com/dwrtldhxd/image/upload/v1734317953/Casas_Lote_Ensue%C3%B1o_Clusters_Reserva_BR_2Ago24_baja_srqv68.jpg",
-  "https://res.cloudinary.com/dwrtldhxd/image/upload/v1734317953/Acceso_lote_Ensue%C3%B1o_Clusters_Reserva_BR_19Jul24_baja_rifpzb.jpg",
-  "https://res.cloudinary.com/dwrtldhxd/image/upload/v1734317953/Vista_a%C3%A9rea_1_Lote_Ensue%C3%B1o_Clusters_Reserva_BR_19Ago24_baja_kznqnr.jpg",
-  "https://res.cloudinary.com/dwrtldhxd/image/upload/v1734317953/Rotonda_Lote_Ensue%C3%B1o_Clusters_Reserva_BR_2Ago24_baja_axccpu.jpg",
+  "https://res.cloudinary.com/dwrtldhxd/image/upload/v1736497566/Acceso_Lote_11_Clusters_REserva_BR_7Ago24_a_def_qj6bxx.jpg",
+  "https://res.cloudinary.com/dwrtldhxd/image/upload/v1736497567/Vista_a%C3%A9rea_2_Lote_11_Clusters_REserva_BR_9Ago24_def_rus6qj.jpg",
+  "https://res.cloudinary.com/dwrtldhxd/image/upload/v1736497566/Casas_Lote_11_Clusters_Reserva_BR_2Ago24_def_pvledy.jpg"
 ];
 
 export const Amenities_demos = [
