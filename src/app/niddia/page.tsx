a15bd9d68b752c42e7eb7fd8a44c1add55469df2
Oasis Creativa <roberto.carpinteyro96@gmail.com>
2025-05-04 22:31:41 -0600

    update

--- src/app/niddia/page.tsx
+++ src/app/niddia/page.tsx
@@ -5,7 +5,13 @@ import Image from "next/image";
 import { useRouter } from "next/navigation"; // Cambia a "next/navigation"
 
 export interface PageAboutProps {}
-
+const chatbotURLs: Record<string, string> = {
+  departamento: "https://interfaces.zapier.com/embed/chatbot/cma0a8w9k002rrdpoow6i6zqf",
+  casa:"https://interfaces.zapier.com/embed/chatbot/casas",
+  oficina: "https://interfaces.zapier.com/embed/chatbot/oficina",
+  lotes: "https://interfaces.zapier.com/embed/chatbot/cma0cvuj7002ll4101gdi416d",
+  niddia: "https://interfaces.zapier.com/embed/chatbot/niddia",
+};
 const NiddiaStandAlone: FC<PageAboutProps> = ({}) => {
   const router = useRouter();
 
@@ -18,6 +24,21 @@ const NiddiaStandAlone: FC<PageAboutProps> = ({}) => {
       router.refresh(); // Forzar recarga en sistemas de app directory
     }
   }, [router]);
+  const buildZapierEmbedHTML = () => {
+    // Determinar la URL del chatbot en función de instruction
+    const iframeURL =
+      "https://interfaces.zapier.com/embed/chatbot/default";
+
+    return `
+      <iframe
+        src="${iframeURL}
+        height="600px"
+        width="100%"
+        allow="clipboard-write *"
+        style="border: none;">
+      </iframe>
+    `;
+  };
 
   return (
     <div>
