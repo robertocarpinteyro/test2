c489535c1e6f329286bc6ad846f16bf2d30a25a8
Oasis Creativa <roberto.carpinteyro96@gmail.com>
2025-05-12 08:28:45 -0600

    update

--- src/app/niddia/page.tsx
+++ src/app/niddia/page.tsx
@@ -1,20 +1,10 @@
 "use client";
 
-import React, { FC, useEffect } from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
-interface NiddiaStandAloneProps {
-  iframeSrc?: string; // Prop para recibir el src del iframe
-  indexValue?: string;
-  selectedOption?: string;
-}
-
-const NiddiaStandAlone: FC<NiddiaStandAloneProps> = ({
-  iframeSrc,
-  indexValue,
-  selectedOption,
-}) => {
+const NiddiaStandAlone = () => {
   const router = useRouter();
 
   useEffect(() => {
@@ -26,23 +16,6 @@ const NiddiaStandAlone: FC<NiddiaStandAloneProps> = ({
       router.refresh(); // Forzar recarga en sistemas de app directory
     }
   }, [router]);
-
-  const chatbotURLs: Record<string, string> = {
-    departamento: "https://interfaces.zapier.com/embed/chatbot/cma0a8w9k002rrdpoow6i6zqf",
-    casa: "https://interfaces.zapier.com/embed/chatbot/casas",
-    oficina: "https://interfaces.zapier.com/embed/chatbot/oficina",
-    lotes: "https://interfaces.zapier.com/embed/chatbot/cma0cvuj7002ll4101gdi416d",
-    niddia: "https://interfaces.zapier.com/embed/chatbot/niddia",
-  };
-
-  const buildIframeURL = () => {
-    // Determinar la URL del iframe
-    return (
-      iframeSrc || // Usar el iframeSrc si se proporciona
-      chatbotURLs[indexValue || ""] || // Usar la URL basada en indexValue
-      "https://interfaces.zapier.com/embed/chatbot/default" // URL predeterminada
-    );
-  };
 
   return (
     <div>
